1a57e481ee3af52dadc2869adc084f31d294c11e
Noah <noah3156@icloud.com>
2025-11-05 16:53:05 -0800

    Initial Commit

---
+++ GitTest1-Noah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitTest1-Noah
+//
+//  Created by Noah N Segura on 11/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitTest1-Noah/GitTest1_NoahApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTest1_NoahApp.swift
+//  GitTest1-Noah
+//
+//  Created by Noah N Segura on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GitTest1_NoahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
